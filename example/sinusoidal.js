7a928934e521396c1189863fe6df66a88a1a2a6f
Demonstrandum <knutsen@jetspace.co>
2018-08-01 01:55:51 +0100

    Added XO linting and accidental forced restyling...

--- example/sinusoidal.js
+++ example/sinusoidal.js
@@ -1,22 +1,24 @@
 import * as BC from '../lib/BasicCanvas.js';
 
-
-let sketch = BC.canvas_id('sketch');
+const sketch = BC.canvas_id('sketch');
 sketch.dimensions(400, 400);
 
 sketch.stroke = BC.Color(0);
 sketch.stroke_weight = 3;
 sketch.stroke_cap = 'round';
 
-
 let i = 0;
 sketch.loop(() => {
-  sketch.background(BC.Color(255, 255, 110));
-  for (let j = 0; j < 8; j++) {
-    sketch.shape('sine', (shape) => {
-      for (let x = 0; x < 3*Math.PI; x += 0.1)
-        shape.vertex(BC.Point(32*x + 50, 32*Math.sin(i)*Math.sin(x+j+i) + 100 + 30*j));
-    });
-  }
-  i += 0.1;
+	sketch.background(BC.Color(255, 255, 110));
+	for (let j = 0; j < 8; j++) {
+		sketch.shape('sine', shape => {
+			for (let x = 0; x < 3 * Math.PI; x += 0.1) {
+				shape.vertex(BC.Point(
+					32 * x + 50,
+					32 * Math.sin(i) * Math.sin(x + j + i) + 100 + 32 * j
+				));
+			}
+		});
+	}
+	i += 0.1;
 });
--- example/still_shapes.js
+++ example/still_shapes.js
@@ -1,39 +1,38 @@
 import * as BC from '../lib/BasicCanvas.js';
 
+// Checker pattern
+import {rectangle} from '../lib/BasicShapes.js';
 
 // Awkwardly patch console.log(...)
-const clone = (obj) => {
-  return Object.assign(
-    Object.create(
-      Object.getPrototypeOf(
-        obj)), obj);
-}
-let console_clone = clone(console);
+
+const console_clone = BC.clone(console);
 
 console.log = (...args) => {
-  return console_clone.log(...(args.map(e => {
-    if (e) return e.valueOf();
-    return e;
-  })));
+	return console_clone.log(...(args.map(e => {
+		if (e) {
+			return e.valueOf();
+		}
+		return e;
+	})));
 };
-
 
 /* Get sketching: */
 
-let sketch = BC.canvas_id('sketch');
+const sketch = BC.canvas_id('sketch');
 sketch.dimensions(400, 400);
 
 sketch.fill = BC.Color(255, 255, 110);
 sketch.background();
 
-
 // Draw a line (vertical)
-for (let i = 60; i < 300; i++)
-  sketch.color(BC.Point(80, i), BC.Color(0,0,0));
+for (let i = 60; i < 300; i++) {
+	sketch.color(BC.Point(80, i), BC.Color(0, 0, 0));
+}
 
 // Draw a parabola
-for (let x = -20; x <= 20; x += 0.01)
-  sketch.color(BC.Point(10*x + 200, -10*x**2 + 300), BC.Color(255, 0, 100));
+for (let x = -20; x <= 20; x += 0.01) {
+	sketch.color(BC.Point(10 * x + 200, -10 * x ** 2 + 300), BC.Color(255, 0, 100));
+}
 
 // Draw a sine wave using vertices (low res)
 sketch.stroke = BC.Color(255);
@@ -41,30 +40,27 @@ sketch.stroke_weight = 3;
 sketch.fill = BC.Color(150, 255, 190, 100);
 
 const PI = 3.14159265;
-sketch.shape('sine', (shape) => {
-  for (let x = 0; x < 3*PI; x += 0.2)
-    shape.vertex(BC.Point(32*x + 50, 32*Math.sin(x) + 100));
+sketch.shape('sine', shape => {
+	for (let x = 0; x < 3 * PI; x += 0.2) {
+		shape.vertex(BC.Point(32 * x + 50, 32 * Math.sin(x) + 100));
+	}
 }).close().fill();
-// sketch.shapes['sine'].shape.close(); // Or do this
+// Sketch.shapes['sine'].shape.close(); // Or do this
 
-let c = sketch.color(BC.Point(90,110));
+const c = sketch.color(BC.Point(90, 110));
 console.log(c);
 
-
-// Checker pattern
-import { rectangle } from '../lib/BasicShapes.js'
-
 sketch.fill = BC.Color(0, 10);
-sketch.stroke = BC.Color(0, 0, 0, 0)
+sketch.stroke = BC.Color(0, 0);
 
-let d = 50;
-for (let r = 0; r < sketch.width/d; r++) {
-  for (let c = 0; c < sketch.height/d; c++) {
-    if ((r+c) % 2 == 0) {
-      sketch.shape(null, rectangle(BC.Point(r*d, c*d), d, d));
-    }
-  }
+const d = 50;
+for (let r = 0; r < sketch.width / d; r++) {
+	for (let c = 0; c < sketch.height / d; c++) {
+		if ((r + c) % 2 === 0) {
+			sketch.shape(null, rectangle(BC.Point(r * d, c * d), d, d));
+		}
+	}
 }
 
 // All our shapes
-console.log(sketch.shapes)
+console.log(sketch.shapes);
