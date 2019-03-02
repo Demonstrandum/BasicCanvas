c5e48712c7b10e7bedd28a2c8f9ed42fbc77d5ed
Demonstrandum <knutsen@jetspace.co>
2019-03-02 17:31:44 +0000

    File routing..?

--- index.py
+++ index.py
@@ -6,4 +6,6 @@ PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
 class handler(Handler):
-    pass
+    def do_GET(self):
+        httpd = socketserver.TCPServer(("", 8000), Handler)
+        httpd.server_forever()
