08a8d47389757a4ce98d519031856eb0a3dd3d29
Demonstrandum <moi@knutsen.co>
2019-10-24 10:52:55 +0100

    Added more helpful prototypes and cleaned up spring pendulum

--- example/spring_pendulum.js
+++ example/spring_pendulum.js
@@ -1,43 +1,62 @@
-import { canvas, Point, HEX, HSLA } from '../lib/BasicCanvas.js';
+import { canvas, Point, HEX, HSLA, css } from '../lib/BasicCanvas.js';
 import { ellipse } from '../lib/BasicShapes.js';
 
-const [sin, cos] = [Math.sin, Math.cos]; 
+const [sin, cos] = [Math.sin, Math.cos];
 
 const g = 9.81 / 60;
 
-let m = 3;
-let k = Math.random()*0.01 + 0.01;
-let x = (Math.random()-0.5)*20 + 5;
-let theta = (Math.random()-0.5) * Math.PI;
-let omega = Math.random()*0.02 + 0.02;
-let v_x = Math.random()*2 + 0.1;
+let m = 3 * (0.4 + Math.random());
+let k = 0.01 * (1 + Math.random());
+let ext = (Math.random() - 0.5) * 20 + 5;
+let theta = (Math.random() - 0.5) * 2*Math.PI;
+let omega = 0.02 * (1 + Math.random());
+let v_ext = Math.random() + 0.1;
 let L = 100;
-const a_x = () => (L+x) * omega * omega + g * cos(theta) - (k * x)/m
-const alpha = () => -g/(L+x) * sin(theta) - 2 * (omega * v_x)/(L+x)
+const a_ext = () => (L+ext) * omega * omega + g * cos(theta) - (k * ext)/m
+const alpha = () => -g/(L+ext) * sin(theta) - 2 * (omega * v_ext)/(L+ext)
 
 const sketch = canvas(document.getElementById('sketch'));
-[sketch.width, sketch.height] = [400, 400];
+[sketch.width, sketch.height] = [420, 480];
 sketch.translate(sketch.width / 2, sketch.height / 3);
+
+document.body.html`<p id="equ">
+    \begin{align}
+      \ddot{\vartheta} &= -\frac{g}{l + x}\sin \vartheta - \frac{2\dot{x}}{l + x}\dot{\vartheta}
+      \\\\
+      \ddot{x} &= (l + x)\dot{\vartheta}^2 - \frac{kx}{m} + g \cos \vartheta
+    \end{align}
+  </p>
+`;
+css`
+  #equ {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    padding-bottom: 40px;
+    margin-top: ${sketch.height / 1.25}px;
+  }
+`;
 
 const BG = HEX('#cdf');
 sketch.stroke_cap = 'round';
 
-let coord = Point(0, L);
+let wire_end = Point(0, L);
 const trail = [];
 
-sketch.loop(() => {
+sketch.loop(frame => {
     sketch.background(BG);
     sketch.fill = 'transparent';
     sketch.render('trail', shape => {
       sketch.stroke_weight = 1;
       let transparency = 1;
       for (const point of trail) {
-        sketch.stroke = HSLA(transparency * (360 / 255), 100, 50, transparency);
+        sketch.stroke = HSLA(frame + transparency * (360 / 255), 100, 50, transparency);
         shape.vertex(point);
-        transparency += 1;
+        transparency += 0.75;
       }
     });
-    if (trail.length > 255) {
+    if (trail.length > 255 / 0.75) {
       trail.shift();  // We'll start deleting the end of the trail at this point.
     }
 
@@ -49,27 +68,31 @@ sketch.loop(() => {
             shape.vertex(i, 6*sin(i*14*Math.PI));
         }
     });
-    spring.scale(L+x - 20, 1);  // Make room for the line at the bottom.
-    spring.vertex(L+x, spring.vertices.slice(-1)[0][1]);  // Adds the little line on the end.
+    spring.scale(L+ext - 20, 1);  // Make room for the line at the bottom.
+    spring.vertex(L+ext, spring.vertices.last.y);  // Adds the little line on the end.
     spring.translate(20, 0);  // Make room for line at the top.
-    spring.vertices.unshift([0, 0]);  // Add on a (0, 0) vertex for the line on the top.
+    spring.prepend_vertex(0, 0);  // Add on a (0, 0) vertex for the line on the top.
     spring.rotate(-theta + Math.PI/2);  // Rotate the spring to match bob motion.
 
     spring.render();
 
-    coord = Point(...spring.vertices.slice(-1)[0]);  // The bob is at the end of the spring.
-    
+    wire_end = Point(...spring.vertices.slice(-1)[0]);  // The bob is at the end of the spring.
 
     sketch.fill = HEX('#000000aa');
     sketch.render('origin', ellipse(Point(0,0), 3));
-    sketch.render('bob', ellipse(coord, 10));
+    sketch.render('bob', ellipse(wire_end, m * 3.5));
 
-    v_x += a_x();
-    let new_x = x;
-    new_x += v_x;
+    sketch.stroke_weight = 0;
+    sketch.fill = HEX('#000e')
+    sketch.font = 'serif';
+    sketch.text(`m = ${m.roundTo(3)} kg`, wire_end.add(Point(20, 0)))
+
+    v_ext += a_ext();
+    let new_ext = ext;
+    new_ext += v_ext;
     omega += alpha();
     theta += omega;  // Euler Integration.
-    x = new_x;
+    ext = new_ext;
 
-    trail.push(coord);
+    trail.push(wire_end);
 });
